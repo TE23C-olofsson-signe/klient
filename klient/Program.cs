c190f9f2179c41c243f0e6542e9b770fdbb7cbcb
Signe Olofsson <signe.olofsson@elev.ga.ntig.se>
2026-01-19 12:51:59 +0100

    first commit

---
+++ klient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json; 
+
+string t = File.ReadAllText("tool.json"); 
+List<Tool> tools = JsonSerializer.Deserialize<List<Tool>>(t); 
+
+foreach (Tool tool in tools)
+{
+    Console.WriteLine(tool.Name);
+}
+
+Console.ReadLine(); 
+
+// Tool h = new Tool(){ Name = "Hammare", Weight = 1}; 
+
+// string hText=JsonSerializer.Serialize(h); 
+
+// File.WriteAllText("tool.json",hText); 
+class Tool
+{
+    public string Name { get; set; }
+    public int Weight { get; set; }
+}

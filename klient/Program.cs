82023e087d0e2ad6d2f06ff6212c08600483b356
Signe Olofsson <signe.olofsson@elev.ga.ntig.se>
2026-01-21 14:51:43 +0100

    tråk

--- klient/Program.cs
+++ klient/Program.cs
@@ -1,22 +1,43 @@
-﻿using System.Text.Json; 
+﻿//Skapa en request
+//Skicka request
+//Ta emot svar, läsa text
+//Deserialisera text
+//Skriv ut data
 
-string t = File.ReadAllText("tool.json"); 
-List<Tool> tools = JsonSerializer.Deserialize<List<Tool>>(t); 
+using System.Net.Http.Json;
 
-foreach (Tool tool in tools)
-{
-    Console.WriteLine(tool.Name);
-}
+HttpClient client = new(); 
+client.BaseAddress = new Uri("https://pokeapi.co/api/v2/"); 
+HttpResponseMessage message=client.GetAsync("pokemon/snorlax").Result;  
+
+//Console.WriteLine(message.Content.ReadAsStringAsync().Result);
+
+pokemon p = message.Content.ReadFromJsonAsync<pokemon>().Result; 
+
+Console.WriteLine(p.Weight);
 
 Console.ReadLine(); 
 
-// Tool h = new Tool(){ Name = "Hammare", Weight = 1}; 
 
-// string hText=JsonSerializer.Serialize(h); 
+// using System.Text.Json; 
 
-// File.WriteAllText("tool.json",hText); 
-class Tool
-{
-    public string Name { get; set; }
-    public int Weight { get; set; }
-}
+// string t = File.ReadAllText("tool.json"); 
+// List<Tool> tools = JsonSerializer.Deserialize<List<Tool>>(t); 
+
+// foreach (Tool tool in tools)
+// {
+//     Console.WriteLine(tool.Name);
+// }
+
+// Console.ReadLine(); 
+
+// // Tool h = new Tool(){ Name = "Hammare", Weight = 1}; 
+
+// // string hText=JsonSerializer.Serialize(h); 
+
+// // File.WriteAllText("tool.json",hText); 
+// class Tool
+// {
+//     public string Name { get; set; }
+//     public int Weight { get; set; }
+// }
---
+++ klient/pokemon.cs
@@ -0,0 +1,5 @@
+public class pokemon
+{
+    public string Name { get; set; }
+    public int Weight { get; set; }
+}
